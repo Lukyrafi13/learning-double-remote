16ec266acdbe13138f902dde9dabc4cc564c3483
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2023-08-30 13:38:54 +0700

    rfcollateral

--- NewLMS.Umkm.API/Controllers/RFColLateralBC/RFColLateralBCController.cs
+++ NewLMS.Umkm.API/Controllers/RFColLateralBC/RFColLateralBCController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using NewLMS.UMKM.Data.Dto.RFColLateralBCs;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
