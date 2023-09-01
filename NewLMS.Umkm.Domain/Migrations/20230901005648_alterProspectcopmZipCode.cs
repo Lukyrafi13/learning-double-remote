6f47ff2e63860c2c5f18cdc0ef97407349a4ed34
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2023-09-01 08:28:40 +0700

    fixing prospect post and put command

---
+++ NewLMS.Umkm.Domain/Migrations/20230901005648_alterProspectcopmZipCode.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewLMS.UMKM.Domain.Migrations
+{
+    public partial class alterProspectcopmZipCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LoanApplicationCollaterals_RFDocument_RFDocumentId",
+                table: "LoanApplicationCollaterals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LoanApplicationCollaterals_RFMappingAgunan2_RfMappingCollateralId",
+                table: "LoanApplicationCollaterals");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyZipCodeId",
+                table: "Prospects",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LoanApplicationCollaterals_RFDocuments_RFDocumentId",
+                table: "LoanApplicationCollaterals",
+                column: "RFDocumentId",
+                principalTable: "RFDocuments",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LoanApplicationCollaterals_RFMappingAgunan2s_RfMappingCollateralId",
+                table: "LoanApplicationCollaterals",
+                column: "RfMappingCollateralId",
+                principalTable: "RFMappingAgunan2s",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LoanApplicationCollaterals_RFDocuments_RFDocumentId",
+                table: "LoanApplicationCollaterals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_LoanApplicationCollaterals_RFMappingAgunan2s_RfMappingCollateralId",
+                table: "LoanApplicationCollaterals");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompanyZipCodeId",
+                table: "Prospects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LoanApplicationCollaterals_RFDocument_RFDocumentId",
+                table: "LoanApplicationCollaterals",
+                column: "RFDocumentId",
+                principalTable: "RFDocument",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LoanApplicationCollaterals_RFMappingAgunan2_RfMappingCollateralId",
+                table: "LoanApplicationCollaterals",
+                column: "RfMappingCollateralId",
+                principalTable: "RFMappingAgunan2",
+                principalColumn: "Id");
+        }
+    }
+}
