7472db68dc8eef8f5593384d5294247be81af391
Edi Ruhiat <edrhiat@gmail.com>
2023-09-04 11:56:07 +0700

    fix: update prospect put request

--- NewLMS.UMKM.Data/Dto/Prospects/ProspectPutRequest.cs
+++ NewLMS.UMKM.Data/Dto/Prospects/ProspectPutRequest.cs
@@ -3,6 +3,6 @@ namespace NewLMS.UMKM.Data.Dto.Prospects
 {
     public class ProspectPutRequest : ProspectPostRequest
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
     }
 }
