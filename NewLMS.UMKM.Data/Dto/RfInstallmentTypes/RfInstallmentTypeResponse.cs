8b758b2d13304d0ffcc3fd79ffba7def9a6a2c8b
Edi Ruhiat <edlr@windowslive.com>
2023-09-05 13:56:16 +0700

    feat: rf installment type feature

---
+++ NewLMS.UMKM.Data/Dto/RfInstallmentTypes/RfInstallmentTypeResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLMS.UMKM.Data.Dto.RfInstallmentTypes
+{
+    public class RfInstallmentTypeResponse : BaseResponse
+    {
+        public string InstallmentTypeCode { get; set; }
+        public string InstallmentTypeDesc { get; set; }
+    }
+
+    public class RfInstallmentTypeSimpleResponse
+    {
+        public string InstallmentTypeCode { get; set; }
+        public string InstallmentTypeDesc { get; set; }
+    }
+}
---
+++ NewLMS.Umkm.API/Controllers/RfInstalmentType/RfInstallmentTypeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewLMS.UMKM.Common.GenericRespository;
+using NewLMS.UMKM.Data.Dto.RfInstallmentTypes;
+using NewLMS.UMKM.MediatR.Features.RfInstallmentTypes.Queries.GetFilterRfInstalmetTypes;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NewLMS.UMKM.API.Controllers.RfInstallmentType
+{
+    public class RfInstallmentTypeController : BaseController
+    {
+        /// <summary>
+        /// GetFilter RfInstallmentType
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost("Get")]
+        [ProducesResponseType(type: typeof(PagedResponse<IEnumerable<RfInstallmentTypeResponse>>), statusCode: StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetFilterRfCreditNature(RfInstallmentTypesGetFilterQuery command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
