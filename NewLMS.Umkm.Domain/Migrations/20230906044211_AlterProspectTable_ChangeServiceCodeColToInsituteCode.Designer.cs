c5e6455ae27a41f7642ca63527af67611eaef619
Edi Ruhiat <edlr@windowslive.com>
2023-09-06 11:43:08 +0700

    fix: prospect institue

--- NewLMS.Umkm.Domain/Migrations/20230906044211_AlterProspectTable_ChangeServiceCodeColToInsituteCode.Designer.cs
+++ NewLMS.Umkm.Domain/Migrations/20230906044211_AlterProspectTable_ChangeServiceCodeColToInsituteCode.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NewLMS.UMKM.Domain.Context;
 
@@ -11,9 +12,10 @@ using NewLMS.UMKM.Domain.Context;
 namespace NewLMS.Umkm.Domain.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20230906044211_AlterProspectTable_ChangeServiceCodeColToInsituteCode")]
+    partial class AlterProspectTable_ChangeServiceCodeColToInsituteCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ NewLMS.Umkm.Domain/Migrations/20230906044211_AlterProspectTable_ChangeServiceCodeColToInsituteCode.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewLMS.Umkm.Domain.Migrations
+{
+    public partial class AlterProspectTable_ChangeServiceCodeColToInsituteCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prospects_RfInstituteCode_ServiceCodeId",
+                table: "Prospects");
+
+            migrationBuilder.RenameColumn(
+                name: "ServiceCodeId",
+                table: "Prospects",
+                newName: "InstituteCode");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Prospects_ServiceCodeId",
+                table: "Prospects",
+                newName: "IX_Prospects_InstituteCode");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prospects_RfInstituteCode_InstituteCode",
+                table: "Prospects",
+                column: "InstituteCode",
+                principalTable: "RfInstituteCode",
+                principalColumn: "ServiceCode");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Prospects_RfInstituteCode_InstituteCode",
+                table: "Prospects");
+
+            migrationBuilder.RenameColumn(
+                name: "InstituteCode",
+                table: "Prospects",
+                newName: "ServiceCodeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Prospects_InstituteCode",
+                table: "Prospects",
+                newName: "IX_Prospects_ServiceCodeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Prospects_RfInstituteCode_ServiceCodeId",
+                table: "Prospects",
+                column: "ServiceCodeId",
+                principalTable: "RfInstituteCode",
+                principalColumn: "ServiceCode");
+        }
+    }
+}
