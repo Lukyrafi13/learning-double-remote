bf5b7185b7224b7c7c1f5baa7310bc059686ab7d
Edi Ruhiat <edlr@windowslive.com>
2023-09-13 14:01:27 +0700

    fix: rename project NewLMS.Umkm.FileUpload

---
+++ NewLMS.Umkm.FileUpload/Models/UploadResponseModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NewLMS.UMKM.FileUpload.Models
+{
+    public class UploadResponseModel
+    {
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("data")]
+        public Uri Data { get; set; }
+
+        [JsonPropertyName("statusCode")]
+        public long StatusCode { get; set; }
+
+        [JsonPropertyName("errors")]
+        public object Errors { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+    }
+}
