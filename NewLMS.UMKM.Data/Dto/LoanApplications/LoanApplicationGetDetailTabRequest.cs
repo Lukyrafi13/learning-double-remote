8f1daa15f9dd138f5111e3c4d02c91e563a99cc4
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2023-09-14 00:55:16 +0700

    Prescreening Controller

--- NewLMS.UMKM.Data/Dto/LoanApplications/LoanApplicationGetDetailTabRequest.cs
+++ NewLMS.UMKM.Data/Dto/LoanApplications/LoanApplicationGetDetailTabRequest.cs
@@ -6,5 +6,11 @@ namespace NewLMS.UMKM.Data.Dto.LoanApplications
         public Guid Id { get; set; }
         public string Tab { get; set; } = "initial_data_entry";
     }
+
+    public class LoanApplicationGetPrescreeningDetailTabRequest
+    {
+        public Guid Id { get; set; }
+        public string Tab { get; set; }
+    }
 }
 
