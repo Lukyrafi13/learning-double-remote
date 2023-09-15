fb999cf8e58a15e3174aca555b804ecd8c24b305
Edi Ruhiat <edlr@windowslive.com>
2023-09-15 08:37:08 +0700

    fix: connection string & sikp query

--- NewLMS.UMKM.Data/Dto/SIKPs/SIKPBaseResponse.cs
+++ NewLMS.UMKM.Data/Dto/SIKPs/SIKPBaseResponse.cs
@@ -5,12 +5,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NewLMS.UMKM.Data.Dto.SIKPs
 {
-	public class SIKPBaseResponse
-	{
+    public class SIKPBaseResponse
+    {
         public Guid Id { get; set; }
         public string RegistrationNumber { get; set; }
         public SIKPRequestResponse SIKPRequest { get; set; }
         public SIKPResponseResponse SIKPResponse { get; set; }
+    }
+
+    public class SIKPTableResponse : BaseResponse
+    {
+        public Guid Id { get; set; }
+        public string Fullname { get; set; }
+        public string DataSource { get; set; }
     }
 }
 
