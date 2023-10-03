09ab8f9088dcf24b5f4e8061df305c49f37de65e
Edi Ruhiat <edlr@windowslive.com>
2023-10-03 17:37:39 +0700

    fix: loan application credit history post command

--- NewLMS.Umkm.API/Helpers/Mapping/Transactions/LoanApplicationCreditHistoryProfile.cs
+++ NewLMS.Umkm.API/Helpers/Mapping/Transactions/LoanApplicationCreditHistoryProfile.cs
@@ -9,7 +9,16 @@ namespace NewLMS.Umkm.API.Helpers.Mapping.Transactions
     {
         public LoanApplicationCreditHistoryProfile()
         {
-            CreateMap<LoanApplicationCreditHistory, LoanApplicationCreditHistoryResponse>();
+            CreateMap<LoanApplicationCreditHistory, LoanApplicationCreditHistoryResponse>()
+                .ForMember(d => d.EconomoySectorDetail, s => s.MapFrom(d => d.RfSandiBIEconomySectorClass))
+                .ForMember(d => d.BehaviourDetail, s => s.MapFrom(d => d.RfSandiBIBehaviourClass))
+                .ForMember(d => d.ApplicationTypeDetail, s => s.MapFrom(d => d.RfSandiBIApplicationTypeClass))
+                .ForMember(d => d.CollectibilityDetail, s => s.MapFrom(d => d.RfSandiBICollectibilityClass))
+                .ForMember(d => d.BankDetail, s => s.MapFrom(d => d.RfBank))
+                .ForMember(d => d.RfCondition, s => s.MapFrom(d => d.RfCondition))
+                .ForMember(d => d.CreditTypeDetail, s => s.MapFrom(d => d.RfCreditType));
+            CreateMap<LoanApplicationCreditHistoryPostRequest, LoanApplicationCreditHistory>();
+            CreateMap<LoanApplicationCreditHistoryPutRequest, LoanApplicationCreditHistory>();
         }
     }
 }
