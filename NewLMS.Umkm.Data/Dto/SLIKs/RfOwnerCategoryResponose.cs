7804e888db1874badf00fd89124a1bd326d02231
Edi Ruhiat <edlr@windowslive.com>
2023-10-03 11:16:19 +0700

    fix: slik response

---
+++ NewLMS.Umkm.Data/Dto/SLIKs/RfOwnerCategoryResponose.cs
@@ -0,0 +1,6 @@
+﻿namespace NewLMS.Umkm.Data.Dto.SLIKs
+{
+    public class RfOwnerCategoryResponose
+    {
+    }
+}
--- NewLMS.Umkm.Data/Dto/SLIKs/SLIKRequestResponse.cs
+++ NewLMS.Umkm.Data/Dto/SLIKs/SLIKRequestResponse.cs
@@ -6,6 +6,7 @@ using NewLMS.Umkm.Data.Dto.RfParameterDetails;
 using NewLMS.Umkm.Data.Dto.RfStages;
 using NewLMS.Umkm.Data.Dto.SLIKRequestDebtors;
 using NewLMS.Umkm.Data.Dto.LoanApplications;
+using NewLMS.Umkm.Data.Dto.RfProducts;
 
 namespace NewLMS.Umkm.Data.Dto.SLIKs
 {
@@ -23,11 +24,25 @@ namespace NewLMS.Umkm.Data.Dto.SLIKs
         public double TotalWorkingCapital { get; set; }
         public DateTime? InquiryDate { get; set; }
 
+        public SLIKInfoResponse Info { get; set; }
         public LoanApplicationBaseTabResponse LoanApplication { get; set; }
         public RfParameterDetailResponse RfOwnerCategory { get; set; }
         public RfBranchResponse Branch { get; set; }
         public RfStageResponse RfStage { get; set; }
         public ICollection<SLIKRequestDebtorResponse> SLIKRequestDebtors { get; set; }
+    }
+
+    public class SLIKInfoResponse
+    {
+        public RfBranchResponse BookingBranch { get; set; }
+        public RfParameterDetailResponse OwnerCategory { get; set; }
+        public string AccountOfficer { get; set; }
+        public string LoanApplicationId { get; set; }
+        public RfProductResponse RfProduct { get; set; }
+        public string Fullname { get; set; }
+        public string NoIdentity { get; set; }
+        public string NPWP { get; set; }
+        public DateTime? DateOfBirth { get; set; }
     }
 
     public class SLIKRequestTableResponse
@@ -36,6 +51,8 @@ namespace NewLMS.Umkm.Data.Dto.SLIKs
         public string LoanApplicationId { get; set; }
         public RfBranchResponse Branch { get; set; }
         public RfParameterDetailResponse RfOwnerCategory { get; set; }
+        public string ApplicationStatus { get; set; }
+        public string NoIdentity { get; set; }
         public string Fullname { get; set; }
         public DateTime CreatedDate { get; set; }
         public ICollection<SLIKRequestDebtorResponse> SLIKRequestDebtors { get; set; }
