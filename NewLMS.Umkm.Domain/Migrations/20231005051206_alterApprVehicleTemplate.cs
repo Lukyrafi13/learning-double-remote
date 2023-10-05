54b4446dc4095ce2f2a034f8c05a6234ac85de85
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2023-10-05 12:23:33 +0700

    Add Migrations alter ApprVehicleTemplate

---
+++ NewLMS.Umkm.Domain/Migrations/20231005051206_alterApprVehicleTemplate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewLMS.Umkm.Domain.Migrations
+{
+    public partial class alterApprVehicleTemplate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "OwnershipStatus",
+                table: "ApprVehicleTemplate",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApprVehicleTemplate_OwnershipStatus",
+                table: "ApprVehicleTemplate",
+                column: "OwnershipStatus");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ApprVehicleTemplate_RfParameterDetails_OwnershipStatus",
+                table: "ApprVehicleTemplate",
+                column: "OwnershipStatus",
+                principalTable: "RfParameterDetails",
+                principalColumn: "ParameterDetailId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ApprVehicleTemplate_RfParameterDetails_OwnershipStatus",
+                table: "ApprVehicleTemplate");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ApprVehicleTemplate_OwnershipStatus",
+                table: "ApprVehicleTemplate");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnershipStatus",
+                table: "ApprVehicleTemplate",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
